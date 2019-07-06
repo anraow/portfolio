52ee80dd4e2cb9b1f1b1464ccd9ff3514013d1a2
DamirSovetov <37625616+DamirSovetov@users.noreply.github.com>
2019-07-06 11:31:55 +0300

    adding basic files to my repository

---
+++ script.js
@@ -0,0 +1,60 @@
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btnFilter");
+for (var i = 0; i < btns.length; i++){
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("trigger");
+    current[0].className = current[0].className.replace(" trigger", "");
+    this.className += " trigger";
+  });
+}
+
+// commits slider
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusComments(n){
+  showComments(slideIndex += n);
+}
+
+function showComments(n) {
+  var i;
+  var x = document.getElementsByClassName("comment");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
